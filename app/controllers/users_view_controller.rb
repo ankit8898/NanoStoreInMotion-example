04f7140920a4a5fac05d1fc0c0737df57116f4b0
ankit gupta <ankit.gupta8898@gmail.com>
2012-12-23 19:21:18 +0530

    some refactoring

--- app/controllers/users_view_controller.rb
+++ app/controllers/users_view_controller.rb
@@ -1,26 +1,23 @@
 class UsersViewController < UIViewController
 
-  HEADER_LABEL_TAG = 1
+  HEADER_LABEL_TAG = 5
   NAME_TEXT_FIELD_TAG = 2
   EMAIL_FIELD_TAG = 3
   BUTTON_TAG = 4
 
-   def loadView
+  def loadView
     views = NSBundle.mainBundle.loadNibNamed "UsersViewController", owner:self, options:nil
     self.view = views[0]
-   end
+  end
 
   def viewDidLoad
     @label_top = label_top
-    @text_field_name = text_field1
-    @text_field_mail = text_field2
+    @text_field_name = text_field_name
+    @text_field_mail = text_field_email
     @tap_gesture_recognizer = tap_gesture_recognizer
     @button = button
-    #@label_top.delegate = self
-    self.view.addSubview(@label_top)
-    self.view.addSubview(@text_field)
+    subViewAdder [@label_top,@text_field,@button]
     self.view.addGestureRecognizer(@tap_gesture_recognizer)
-    self.view.addSubview(@button)
   end
 
 
@@ -32,51 +29,52 @@ class UsersViewController < UIViewController
   def tap_gesture_recognizer
     UITapGestureRecognizer.alloc.initWithTarget(self,action:'dismissKeyboard:')
   end
-  
-  def button
-   btn = self.view.viewWithTag(4)
-   btn.addTarget(self, action:'enter', forControlEvents:UIControlEventTouchUpInside)
-  end
 
   def enter
     unless isvalidForm
       User.create_new @text_field_name.text , @text_field_mail.text 
-      showAlert("Success", title:"Added.")
-      p User.all
+      showAlert("Success", title:"Added User.")
     else
      showAlert("Error", title:"Please, fill all the fields.")
    end
-  end
+ end
 
-  def close
+ def close
   dismissModalViewControllerAnimated true
 end
 
-  
-  def isvalidForm
-   @text_field_name.text.empty? || @text_field_mail.text.empty?
-  end
 
-  def showAlert(message, title:title)
+def isvalidForm
+ @text_field_name.text.empty? || @text_field_mail.text.empty?
+end
 
+def showAlert(message, title:title)
   alert = UIAlertView.alloc.initWithTitle(title,
-                      message:message,
-                      delegate:self,
-                      cancelButtonTitle:'OK',
-                      otherButtonTitles:nil)
+    message:message,
+    delegate:self,
+    cancelButtonTitle:'OK',
+    otherButtonTitles:nil)
   alert.show
 end
 
-  def label_top
-    self.view.viewWithTag(5)
-    #label
-  end
+def label_top
+  self.view.viewWithTag HEADER_LABEL_TAG 
+end
 
-  def text_field1
-    self.view.viewWithTag(2)
-  end
+def text_field_name
+  self.view.viewWithTag NAME_TEXT_FIELD_TAG
+end
 
-    def text_field2
-    self.view.viewWithTag(3)
-  end
+def text_field_email
+  self.view.viewWithTag EMAIL_FIELD_TAG
+end
+
+def button
+ btn = self.view.viewWithTag BUTTON_TAG
+ btn.addTarget(self, action:'enter', forControlEvents:UIControlEventTouchUpInside)
+end
+
+def subViewAdder arr 
+  arr.each {|field| self.view.addSubview field}
+end
 end
